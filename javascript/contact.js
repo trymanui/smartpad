1db9671375b7cabf1f2865d71473a405b3b637a7
trymanui <s3965641@gmail.com>
2025-05-20 15:34:08 +0530

    Update contact.js

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,15 +1,15 @@
 document.getElementById('homebtn').addEventListener('click',()=>{
   if(!window.location.href.includes("home.html"))
-    window.location.href="../home.html";
+    window.location.href="home.html";
 });
 
 document.getElementById('savebtn').addEventListener('click',()=>{
 if(!window.location.href.includes("../saved.html"))
-  window.location.href="../saved.html";
+  window.location.href="saved.html";
 });
 
 document.querySelector('.reset').addEventListener('click',()=>{
-window.location.href="../home.html";
+window.location.href="home.html";
 });
 
           
